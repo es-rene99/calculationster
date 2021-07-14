9c57178adbc98ddfa21d40ec607ce0d55d6cc13c
Maxray Savage <MaxraySavage@users.noreply.github.com>
2021-07-14 00:43:24 -0400

    build-problem factory as well as implements leveling up

--- src/main.js
+++ src/main.js
@@ -15,25 +15,16 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (maxInt - minInt) + minInt);
 }
 
-
-// create random operators
-
-function getRandomOperator() {
-  const operators = ['+', '-', '*', '/'];
-  const randomOperator = operators[Math.floor(Math.random() * operators.length)];
-  return randomOperator;
-}
-
-// tracker for ammount of correct answers
-
-let correctResult;
-
 // basic math operands
 
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
+
+/*
+Solve a single operation
+*/
 
 function getCorrectResult(mathOne, mathTwo, operatorValue) {
   switch (operatorValue) {
@@ -54,24 +45,108 @@ function getCorrectResult(mathOne, mathTwo, operatorValue) {
   }
 }
 
+// create random operators
+
+function getRandomOperator() {
+  const operators = ['+', '-', '*', '/'];
+  const randomOperator = operators[Math.floor(Math.random() * operators.length)];
+  return randomOperator;
+}
+
+/*
+A problem factory to create all our problems!
+Feels like I already have too many of these in my life but, oh well...
+a simple Object with an array of operands and an array of operators and the answer
+*/
+
+function problemFactory(operands, operators, answer) {
+  const combinedArray = new Array(operands.length * 2 - 1);
+  for (let i = 0; i < operands.length - 1; i += 1) {
+    combinedArray[2 * i] = operands[i];
+    combinedArray[2 * i + 1] = operators[i];
+  }
+  combinedArray[operands.length * 2 - 2] = operands[operands.length - 1];
+  const text = combinedArray.join(' ');
+  return {
+    operands,
+    operators,
+    text,
+    answer,
+  };
+}
+
+/*
+This function solves the expression you put into it!
+It handles the limited PEMDAS stuff that we need it to as well.
+*/
+
+function expressionSolver(operandArray, operatorArray) {
+  const operands = [...operandArray];
+  const operators = [...operatorArray];
+  while (operands.length > 1) {
+    let nextOperatorIndex = operators.findIndex((o) => ['*', '/'].includes(o));
+    if (nextOperatorIndex === -1) {
+      nextOperatorIndex = operators.findIndex((o) => ['+', '-'].includes(o));
+    }
+    const nextOperator = operators[nextOperatorIndex];
+    const nextOperandIndex = nextOperatorIndex;
+    const operandOne = operands[nextOperatorIndex];
+    const operandTwo = operands[nextOperatorIndex + 1];
+    const answerOfNextOperation = getCorrectResult(operandOne, operandTwo, nextOperator);
+    operands.splice(nextOperandIndex, 2, answerOfNextOperation);
+    operators.splice(nextOperatorIndex, 1);
+  }
+  return operands[0];
+}
+
+/*
+Make a simple problem with some number of n digit operands and a single repeated operator
+For subtraction, avoids negatives.
+For division, avoids fractions.
+*/
+
+function makeSimpleProblem(numDigits, operator, numTerms = 2) {
+  const operands = [];
+  const operators = new Array(numTerms - 1).fill(operator);
+  for (let i = 0; i < numTerms; i += 1) {
+    operands.push(getRandomInt(1, 10 ** numDigits));
+  }
+  if (operator === '-') {
+    operands[0] = operands.reduce(add);
+  } else if (operator === '/') {
+    operands[0] = operands.reduce(multiply);
+  }
+  const answer = expressionSolver(operands, operators);
+  return problemFactory(operands, operators, answer);
+}
+
+// tracker for ammount of correct answers
+
+let correctResult;
+
 let winAnswers = 0;
 
 function askProblem() {
-  let firstNumber = getRandomInt(1, 10);
-  const secondNumber = getRandomInt(1, 10);
-  const operator = getRandomOperator();
-  if (operator === '/') {
-    firstNumber *= secondNumber;
+  const level = Math.floor((winAnswers / 10)) + 1;
+  let operator;
+  let numDigits;
+  const numTerms = Math.max(2, level - 4);
+  if (level < 5) {
+    operator = ['+', '-', '*', '/'][level - 1];
+    numDigits = 1;
+  } else {
+    operator = getRandomOperator();
+    numDigits = 2;
   }
+  const problem = makeSimpleProblem(numDigits, operator, numTerms);
+  const userAnswer = Number(prompt(`What is ${problem.text} equal to?`));
 
-  const answer = Number(prompt(`What is ${firstNumber} ${operator} ${secondNumber} equal to?`));
-  const correctAnswer = getCorrectResult(firstNumber, secondNumber, operator);
-  if (answer === correctAnswer) {
+  if (userAnswer === problem.answer) {
     winAnswers += 1;
-    console.log(`${answer} was the correct answer!\nGood job! Correct answers: ${winAnswers}`);
+    console.log(`${userAnswer} was the correct answer!\nGood job! Correct answers: ${winAnswers}`);
   } else {
     console.log(
-      `Ouch! ${answer} was not the correct answer.\n Try again! (correct : ${correctAnswer})`,
+      `Ouch! ${userAnswer} was not the correct answer.\n Try again! (correct : ${problem.answer})`,
     );
   }
 }
